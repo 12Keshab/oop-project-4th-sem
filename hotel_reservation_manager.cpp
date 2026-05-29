2bbd0cf9b4b7f08d5cf6f16b8cf84bb9f3e90de2
Keshab Rana Chhetri <keeshaab12@gmail.com>
2026-05-29 15:31:49 +0900

    created hotel reservation manager class

---
+++ hotel_reservation_manager.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using name space std;
+
+class HotelReservationManager {
+    public:
+        void makeReservation() {
+            cout << "Making a reservation..." << endl;
+        }
+
+        void cancelReservation() {
+            cout << "Cancelling a reservation..." << endl;
+        }
+
+        void viewReservations() {
+            cout << "Viewing all reservations..." << endl;
+        }
+};
