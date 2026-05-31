75850e4a9b5386aa6f09611ecb933c9de5cd735b
Keshab Rana Chhetri <keeshaab12@gmail.com>
2026-05-31 13:13:07 +0900

    fix layout issue

--- hotel_reservation_manager.cpp
+++ hotel_reservation_manager.cpp
@@ -1,17 +1,2 @@
 #include<iostream>
-using name space std;
-
-class HotelReservationManager {
-    public:
-        void makeReservation() {
-            cout << "Making a reservation..." << endl;
-        }
-
-        void cancelReservation() {
-            cout << "Cancelling a reservation..." << endl;
-        }
-
-        void viewReservations() {
-            cout << "Viewing all reservations..." << endl;
-        }
-};
+#include<string>
